a01ecfcc2eea5dfebea8d53f4d8b963a65e2c652
Diego San Vicente <neidan.arq.s@gmail.com>
2024-11-11 13:06:36 -0600

    Add HighSchoolSweetheart exercise
    To mark as completed the string lesson

---
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,37 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    to_string(name)
+    |> String.trim()
+    |> String.at(0)
+  end
+
+  def initial(name) do
+    first_letter(name)
+    |> String.capitalize()
+    |> Kernel.<>(".")
+  end
+
+  def initials(full_name) do
+    [first | last] = String.split(full_name)
+    "#{initial(first)}" <> " " <> "#{initial(last)}"
+  end
+
+  def pair(full_name1, full_name2) do
+    """
+       ******       ******
+     **      **   **      **
+   **         ** **         **
+  **            *            **
+  **                         **
+  **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
+   **                       **
+     **                   **
+       **               **
+         **           **
+           **       **
+             **   **
+               ***
+                *
+  """
+  end
+end
